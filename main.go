d0c649f1358aaff581acc0c5c7799011585f31d7
tzx <tzx@tzx.local>
2018-07-03 22:22:24 +0800

    添加跨域设置

--- main.go
+++ main.go
@@ -69,29 +69,38 @@ func upload(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetFiles(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm();
-	var pageSize int = 10;
-	var pageNum int = 1;
-	if r.FormValue("pageSize") != "" {
-		pageSize, _ = strconv.Atoi(r.FormValue("pageSize"))
-	}
-	if r.FormValue("pageNum") != "" {
-		pageNum, _ = strconv.Atoi(r.FormValue("pageNum"))
-	}
-	files, err := port.GetFiles(pageSize * (pageNum - 1), pageNum * pageSize)
-	if err != nil {
-		utils.HandleServerError(w, err);
-	}
-	tmp, err := json.Marshal(files);
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Add("Access-Control-Allow-Headers", "x-requested-with")
+	w.Header().Add("Content-Type", "application/x-msdownload")
 
-	wc := 0
-	for wc < len(tmp) {
-		n, err := w.Write(tmp)
-		if err != nil {
-			utils.HandleServerError(w, err);	
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(200)
+	} else {
+		r.ParseForm();
+		var pageSize int = 10;
+		var pageNum int = 1;
+		if r.FormValue("pageSize") != "" {
+			pageSize, _ = strconv.Atoi(r.FormValue("pageSize"))
 		}
-		wc += n;
-	}
+		if r.FormValue("pageNum") != "" {
+			pageNum, _ = strconv.Atoi(r.FormValue("pageNum"))
+		}
+		files, err := port.GetFiles(pageSize * (pageNum - 1), pageNum * pageSize)
+		if err != nil {
+			utils.HandleServerError(w, err);
+		}
+		tmp, err := json.Marshal(files);
+
+		wc := 0
+		for wc < len(tmp) {
+			n, err := w.Write(tmp)
+			if err != nil {
+				utils.HandleServerError(w, err);	
+			}
+			wc += n;
+		}
+    }
 }
 
 func main() {
