b3bb92a588ff45bfd7474aa611cd2f46d4c9aebe
abelce <1061225829@qq.com>
2018-07-03 22:55:54 +0800

    修改跨域

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"io"
-	"os"
-	"github.com/satori/go.uuid"
-	"file/utils"
-	"file/port"
-	"strconv"
 	"encoding/json"
+	"file/port"
+	"file/utils"
+	"fmt"
+	"github.com/satori/go.uuid"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 type handler struct {
@@ -46,61 +46,63 @@ func upload(w http.ResponseWriter, r *http.Request) {
 		file, handler, err := r.FormFile("file")
 
 		if err != nil {
-			utils.HandleHTTPError(w, err);
+			utils.HandleHTTPError(w, err)
 		}
 		defer file.Close()
 		path, _ := uuid.NewV4()
-		f, err := os.OpenFile("/data/upload_files/" + path.String(), os.O_WRONLY|os.O_CREATE, 0666)
+		f, err := os.OpenFile("/data/upload_files/"+path.String(), os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
-			utils.HandleServerError(w, err);
+			utils.HandleServerError(w, err)
 		}
 		defer f.Close()
 		io.Copy(f, file)
 
-		id, _ := uuid.NewV4();
+		id, _ := uuid.NewV4()
 		_, err = port.InsertFile(id.String(), path.String(), handler.Filename, "--", utils.GetTimestamp())
 		if err != nil {
-			utils.HandleServerError(w, err);
+			utils.HandleServerError(w, err)
 			fmt.Fprintln(w, "upload failed123!")
-			return;
+			return
 		}
 		fmt.Fprintln(w, "upload success!")
 	}
 }
 
 func GetFiles(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.Header().Add("Access-Control-Allow-Headers", "x-requested-with")
-	w.Header().Add("Content-Type", "application/x-msdownload")
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	w.Header().Set("access-control-allow-origin", "*")
+	w.Header().Set("access-control-allow-methods", "*")
+	w.Header().Set("access-control-allow-headers", "*")
+	w.Header().Set("access-control-expose-headers", "*")
+	w.Header().Set("access-control-allow-credentials", "true")
 
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(200)
 	} else {
-		r.ParseForm();
-		var pageSize int = 10;
-		var pageNum int = 1;
+		r.ParseForm()
+		var pageSize int = 10
+		var pageNum int = 1
 		if r.FormValue("pageSize") != "" {
 			pageSize, _ = strconv.Atoi(r.FormValue("pageSize"))
 		}
 		if r.FormValue("pageNum") != "" {
 			pageNum, _ = strconv.Atoi(r.FormValue("pageNum"))
 		}
-		files, err := port.GetFiles(pageSize * (pageNum - 1), pageNum * pageSize)
+		files, err := port.GetFiles(pageSize*(pageNum-1), pageNum*pageSize)
 		if err != nil {
-			utils.HandleServerError(w, err);
+			utils.HandleServerError(w, err)
 		}
-		tmp, err := json.Marshal(files);
+		tmp, err := json.Marshal(files)
 
 		wc := 0
 		for wc < len(tmp) {
 			n, err := w.Write(tmp)
 			if err != nil {
-				utils.HandleServerError(w, err);	
+				utils.HandleServerError(w, err)
 			}
-			wc += n;
+			wc += n
 		}
-    }
+	}
 }
 
 func main() {
